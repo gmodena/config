f3ce2d736c78e891ffd603c0dfc1923dbed20137
Gabriele Modena <gmodena@pm.me>
2021-09-26 15:30:09 +0200

    Add git sendemail config

--- profiles/home/personal.nix
+++ profiles/home/personal.nix
@@ -2,5 +2,10 @@
   programs.git = {
     userEmail = "gmodena@pm.me";
     userName = "Gabriele Modena";
+    # for git send-email
+    smtpServer = "smtp.gmail.com";
+    smtpServerPort = "587";
+    smtpEncryption = "tls";
+    smtpUser = "gabriele.modena@gmail.com";
   };
 }
