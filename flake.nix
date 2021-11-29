0ffc4182f7eabb4bde29e977cc01160913c2c663
gmodena <gmodena@wikimedia.org>
2021-11-29 10:27:17 +0100

    Update nixpkgs and home-manager to 21.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "nix system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
