add06896cc20b53d91a272d8acbcec09506c2e83
Gabriele Modena <gmodena@wikimedia.org>
2022-07-12 10:43:37 +0200

    Add barrier server config

--- profiles/home/wmf.nix
+++ profiles/home/wmf.nix
@@ -3,4 +3,9 @@
     userEmail = "gmodena@wikimedia.org";
     userName = "Gabriele Modena";
   };
+
+  xdg.configFile."barrier/barrier.conf" = {
+    # Barrier KVM server configuration.
+    text = builtins.readFile  ../../modules/home/barrier.conf;
+  };
 }
