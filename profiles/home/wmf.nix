76c3cb3dc81b5178fe08f8a6ef3aebc28d247c22
Gabriele Modena <gmodena@wikimedia.org>
2022-08-31 13:22:19 +0200

    Enable verbose git output.

--- profiles/home/wmf.nix
+++ profiles/home/wmf.nix
@@ -2,6 +2,9 @@
   programs.git = {
     userEmail = "gmodena@wikimedia.org";
     userName = "Gabriele Modena";
+    iniContent.core = {
+      sshCommand = "ssh -v";
+    };
   };
 
   xdg.configFile."barrier/barrier.conf" = {
