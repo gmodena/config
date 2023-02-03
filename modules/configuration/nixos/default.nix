d56ec690316613b34678a07f26ff5684a4979b79
Gabriele Modena <gmodena@pm.me>
2023-02-03 20:04:07 +0100

    Remap esc to caps lock

--- modules/configuration/nixos/default.nix
+++ modules/configuration/nixos/default.nix
@@ -44,7 +44,7 @@
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.xkbOptions = "caps:swapescape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
