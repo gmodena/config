c226fac4e6bdb55c37abb24292ea48d86aa9295a
Gabriele Modena <gmodena@pm.me>
2023-02-04 21:50:48 +0100

    Install paperwm on gnome 43

--- modules/home-manager/desktop/nixos/default.nix
+++ modules/home-manager/desktop/nixos/default.nix
@@ -1,4 +1,18 @@
-{ input, config, pkgs, ... }: {
+{ input, config, pkgs, ... }:
+let
+  paperwm-develop = pkgs.gnomeExtensions.paperwm.overrideAttrs (old: {
+    version = "39";
+
+    # 2023-03-04: latest commit in develop. Needed to install on Gnome 43.
+    src = pkgs.fetchFromGitHub {
+      owner = "paperwm";
+      repo = "PaperWM";
+      rev = "f590f8b30f0c1962e2bc18f1a39355b0a72636cb";
+      hash = "sha256-ngyTsls0RYQyepfwvNJDPdlGMRC2+woFCW4RVjsaPRU=";
+    };
+  });
+in
+{
   imports = [../../default.nix ];
   
   home.packages = with pkgs; [
@@ -15,5 +29,6 @@
     weechat
     hexchat
     podman
+    paperwm-develop
   ];
 }
