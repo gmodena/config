7234746508cac990621e86bd52bf843daebd296b
Gabriele Modena <gmodena@pm.me>
2023-07-07 17:58:27 +0200

    Version bump to 23.05. (#38)

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "nix system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
