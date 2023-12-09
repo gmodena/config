f2f7ebf5d4c668f44492a750fc4f87812c1878f1
Gabriele Modena <gmodena@pm.me>
2023-12-09 14:50:51 +0100

    nixpkgs: upgrade to 23.11

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   inputs = {
     flatpaks.url = "github:gmodena/nix-flatpak/main";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
